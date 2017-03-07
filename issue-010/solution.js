500609233b84a3c05ba20db67576f0791b61a8c6
Onikah <mona.kwamboka@gmail.com>
2017-03-08 00:50:05 +0300

    solution: array reduce(2)

--- issue-010/solution.js
+++ issue-010/solution.js
@@ -1,4 +1,12 @@
 // implement your solution here
 function evenOdd(a) {
+ return a.reduce(function (sum, num) {
+  if (num % 2 == 0) {
+   sum = sum + num;
 
+  } else {
+   sum = sum - num;
+  }
+  return sum;
+ }, 0)
 }
