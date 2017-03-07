a31a8e6cfb81a471e7dfc88999890ce8c264fc49
Onikah <mona.kwamboka@gmail.com>
2017-03-07 21:46:17 +0300

    solution: simple grading system

--- issue-004/solution.js
+++ issue-004/solution.js
@@ -1,3 +1,23 @@
 function grade(a) {
     // implement your solution here
+    switch (true) {
+     case a > 0 && a < 30:
+      return "F";
+      break;
+     case a > 29 && a < 40:
+      return "E";
+      break;
+     case a > 39 && a < 50:
+      return "D";
+      break;
+     case a > 49 && a < 60:
+      return "C";
+      break;
+     case a > 59 && a < 80:
+      return "B";
+      break;
+     case a > 79 && a <= 100:
+      return "A";
+      break;
+    }
 }
