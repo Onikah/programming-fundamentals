9fd9da05c349c33f1cc255d1caf6ac4da8beccd3
Onikah <mona.kwamboka@gmail.com>
2017-03-14 19:25:07 +0300

    solution: Drill old and young

--- issue-021/solution.js
+++ issue-021/solution.js
@@ -1,4 +1,15 @@
 // implement your solution here
 function oldAndYoung(students) {
+ var oldest = youngest = students[0].yearOfBirth;
+ var obj = {};
 
+ students.forEach((student) => {
+  oldest = (oldest > student.yearOfBirth) ? student.yearOfBirth: oldest;
+  youngest = (youngest < student.yearOfBirth) ? student.yearOfBirth: youngest;
+
+  if (student.yearOfBirth === oldest) { obj.oldest = student.name; }
+  if (student.yearOfBirth === youngest) { obj.youngest = student.name; }
+ });
+
+ return obj;
 }
