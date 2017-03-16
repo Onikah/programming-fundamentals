2eda786a5d5ca7a4c8ff081406b0424d1e3973d4
Onikah <mona.kwamboka@gmail.com>
2017-03-16 22:04:37 +0300

    solution(2): Drill object values

--- issue-022/solution.js
+++ issue-022/solution.js
@@ -1,11 +1,6 @@
 // implement your solution here
 function objectValues(a) {
- var arr = [];
- for (var key in a) {
-  arr.push(key);
- }
-
- return arr.sort().map(function (value) {
+ return Object.keys(a).sort().map(function (value) {
   return a[value];
  });
 }
